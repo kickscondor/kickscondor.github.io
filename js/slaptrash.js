df1039f6c8a5d001cf3642cff770747b1cd450e4
kickscondor <kicks@kickscondor.com>
2019-05-10 23:36:23 +0000

    Force https for embedly script.

--- js/slaptrash.js
+++ js/slaptrash.js
@@ -269,7 +269,7 @@ function slapcard(dim, url, ele) {
    if (!d.getElementById(id)){
      w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
      var e = d.createElement(n); e.id = id; e.async=1;
-     e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+     e.src = 'https://cdn.embedly.com/widgets/platform.js';
      var s = d.getElementsByTagName(n)[0];
      s.parentNode.insertBefore(e, s);
    }
